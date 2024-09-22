6e6b356e96a5718cfacd53b093c21db5173eca9a
ikumisato <kinoko7971@gmail.com>
2024-09-22 15:08:30 +0900

    jQeryをslick追加

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// ページトップボタンの要素を取得
+const pageTopButton = document.getElementById("js-page-top");
+
+// クリックイベントをアロー関数で設定
+pageTopButton.addEventListener("click", () => {
+  // ページの一番上へスムーズにスクロール
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// $(".mv-photo-big").slick({
+//   dots: true,
+// });
+
+$(".single-item").slick();
